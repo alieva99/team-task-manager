9a3df49c032643146aa6a40805adc4f47165ddeb
при <>
2026-03-24 15:57:03 +0500

    Prepare for deployment on Render

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+ "https://team-task-manager-app.onrender.com",  # URL фронтенда
+        "https://team-task-manager-api.onrender.com",   # URL бэкенда
     ],
     allow_credentials=True,
     allow_methods=["*"],
